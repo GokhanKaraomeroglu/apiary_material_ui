e4c368d8d305916f5b77f431faa5754a9d55621a
Himmel König <itogreniyorum@gmail.com>
2021-09-12 19:34:51 +0300

    Navbar changed

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,27 +1,27 @@
 import React, { useState } from 'react';
 import {Link} from 'react-router-dom';
-import { makeStyles } from '@material-ui/core/styles';
-import AppBar from '@material-ui/core/AppBar';
-import Toolbar from '@material-ui/core/Toolbar';
-import Typography from '@material-ui/core/Typography';
-import Button from '@material-ui/core/Button';
-import IconButton from '@material-ui/core/IconButton';
-import MenuIcon from '@material-ui/icons/Menu';
+// import { makeStyles } from '@material-ui/core/styles';
+// import AppBar from '@material-ui/core/AppBar';
+// import Toolbar from '@material-ui/core/Toolbar';
+// import Typography from '@material-ui/core/Typography';
+// import Button from '@material-ui/core/Button';
+// import IconButton from '@material-ui/core/IconButton';
+// import {MenuIcon} from '@material-ui/icons/Menu';
 
 
-const useStyles = makeStyles((theme) => ({
-	root: {
-		flexGrow: 1,
-	},
-	menuButton: {
-		marginRight: theme.spacing(2),
-	},
-	title: {
-		flexGrow: 1,
-	},
-}));
+// const useStyles = makeStyles((theme) => ({
+// 	root: {
+// 		flexGrow: 1,
+// 	},
+// 	menuButton: {
+// 		marginRight: theme.spacing(2),
+// 	},
+// 	title: {
+// 		flexGrow: 1,
+// 	},
+// }));
 function NavBar() {	
-		const classes = useStyles();
+		// const classes = useStyles();
     return(
 <div>
  <nav className="navbar navbar-expand-lg navbar-warning bg-dark">
@@ -56,7 +56,7 @@ function NavBar() {
 
 
   // return (
-    <div className={classes.root}>
+    {/* <div className={classes.root}>
       <AppBar position="static">
         <Toolbar>
           <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu">
@@ -70,7 +70,8 @@ function NavBar() {
       </AppBar>
     </div>
   // );
-</div>
+*/}
+</div> 
         );
 }
 
